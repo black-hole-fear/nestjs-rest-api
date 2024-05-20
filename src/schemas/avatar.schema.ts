49572dd702d06015a5482ff86315fe8b71db248c
black-hole-fear <macSmith0227@outlook.com>
2024-05-20 08:10:21 +0200

    added avatar, user schema for mongo

---
+++ src/schemas/avatar.schema.ts
@@ -0,0 +1,20 @@
+import { 
+    Prop, 
+    Schema, 
+    SchemaFactory 
+} from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Avatar extends Document {
+    @Prop({ required: true })
+    userId: string;
+
+    @Prop({ required: true })
+    hash: string;
+
+    @Prop({ required: true })
+    data: string;    // base64 encoded image
+}
+
+export const AvatarSchema = SchemaFactory.createForClass(Avatar);
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,17 @@
+import { 
+    Prop, 
+    Schema, 
+    SchemaFactory 
+} from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class User extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    job: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
